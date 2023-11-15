3b29cc390a7e647a440c880e1a24dfa5a829e8ce
Anzhelika <anzhela.starushenko@gmail.com>
2023-11-15 16:54:45 +0100

    added prints

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 def main():
     base = 3
     power = 29
-    print(fast_powering(base, power))
+    fast_powering(base, power)
 
 
 def fast_powering(base, power):
+    starting_base = base
     power_in_binary = bin(power)[2:]
+    print(power_in_binary)
     result = 1
+    dupa = 0b1
     for bit in power_in_binary[::-1]:
         if bit == '1':
             result *= base
             base *= base
-            print()
+            dupa <<= 1
+            print(f"{starting_base}^{dupa} = {base}, current bit = {bit} result = {result}")
         else:
             base *= base
+            dupa <<= 1
+            print(f"{starting_base}^{dupa} = {base}, current bit = {bit} result = {result}")
+    print(f"{starting_base}^{power} = {result}")
     return result
 
 
